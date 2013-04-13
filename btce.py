43e8d8ed3b1ca35303ec7cf674436f4e6e567a9b
wgibb <williamgibb@gmail.com>
2013-04-13 18:42:41 -0400

    Update public api calls to be more error tolerant

--- btce.py
+++ btce.py
@@ -9,23 +9,32 @@ import hmac
 import config
 
 def nonce_generator():
-	fd = open("nonce_state", "r")
-	nonce = int(fd.read())
-	fd.close()
-	while (True):
-		nonce = nonce+1
-		fd = open("nonce_state", "w")
-		fd.write(str(nonce))
-		fd.close()
-		yield nonce
+    fd = open("nonce_state", "r")
+    nonce = int(fd.read())
+    fd.close()
+    while (True):
+        nonce = nonce+1
+        fd = open("nonce_state", "w")
+        fd.write(str(nonce))
+        fd.close()
+        yield nonce
 
 nonce = nonce_generator()
 
+# TODO: support btc_eur, ltc_btc, ltc_usd, nmc_btc, eur_usd
+correct_pairs = [['btc', 'usd'], 
+                ['btc', 'rur'], 
+                ['usd','rur'],
+                ['ltc','usd'],
+                ['btc','eur'],
+                ['ltc','eur'],
+                ['eur','usd'],]
+
 class BTCEError(Exception):
-	def __init__(self, msg):
-		self.msg = msg
-	def __str__(self):
-		return repr(self.msg)
+    def __init__(self, msg):
+        self.msg = msg
+    def __str__(self):
+        return repr(self.msg)
 
 def api_request(method, misc_params = {}):
 	params = {
@@ -66,54 +75,72 @@ def api_request(method, misc_params = {}):
 		raise BTCEError("Unexpected error: " + str(sys.exc_info()[0]))
 
 def pubapi_request(pair, type):
-	url = "https://btc-e.com/api/2/" + pair + "/" + type
-	try:
-		f = urllib.urlopen(url)
-		return json.load(f)
-	except IOError:
-		print f.code()
+    if pair in correct_pairs:
+        combo = pair[0]+'_'+pair[1]
+        print combo
+    else:
+        print 'can nopt find pair'
+        return False
+    print 'buiding_url'
+    url = "https://btc-e.com/api/2/" + combo + "/" + type
+    print url
+    try:
+        f = urllib.urlopen(url)
+        return json.load(f)
+    except IOError:
+        print f.code()
 
-# TODO: support btc_eur, ltc_btc, ltc_usd, nmc_btc, eur_usd
-correct_pairs = [['btc', 'usd'], ['btc', 'rur'], ['usd','rur']]
 
 def ticker(pair):
-	return pubapi_request(pair, "ticker")['ticker']
+    req =  pubapi_request(pair, "ticker")
+    if req:
+        return req['ticker']
+    else:
+        return None
 
 def trades(pair):
-	return pubapi_request(pair, "ticker")['ticker']
+    req =  pubapi_request(pair, "trades")
+    if req:
+        return req['trades']
+    else:
+        return None
 
 def depth(pair):
-	return pubapi_request(pair, "ticker")['ticker']
+    req =  pubapi_request(pair, "depth")
+    if req:
+        return req['depth']
+    else:
+        return None
 
 def getinfo():
-	return api_request('getInfo')
+    return api_request('getInfo')
 
 def order_list(filter = {}):
-	return api_request('OrderList', filter)
+    return api_request('OrderList', filter)
 
 def trans_history(filter = {}):
-	return api_request('TransHistory', filter)
+    return api_request('TransHistory', filter)
 
 def trade_history(filter = {}):
-	return api_request('TradeHistory', filter)
+    return api_request('TradeHistory', filter)
 
 def prepare_trade(from_currency, to_currency, rate, amount):
-	pair = [from_currency, to_currency]
-	for p in correct_pairs:
-		if pair == p:
-			type = 'sell'
-		elif pair == [p[1], p[0]]:
-			type = 'buy'
-			pair = p
-			amount = float(amount) / float(rate)
-	pair = '_'.join(pair)
-	if not type:
-		raise BTCEError("Unsupported currency pair: " + pair[0] + "_" + pair[1])
-	return pair, type, rate, amount
+    pair = [from_currency, to_currency]
+    for p in correct_pairs:
+        if pair == p:
+            type = 'sell'
+        elif pair == [p[1], p[0]]:
+            type = 'buy'
+            pair = p
+            amount = float(amount) / float(rate)
+    pair = '_'.join(pair)
+    if not type:
+        raise BTCEError("Unsupported currency pair: " + pair[0] + "_" + pair[1])
+    return pair, type, rate, amount
 
 def trade(pair, type, rate, amount):
 #	print pair, type, amount, rate
-	return api_request('Trade', { 'pair': pair, 'type': type, 'rate': rate, 'amount': amount })
+    return api_request('Trade', { 'pair': pair, 'type': type, 'rate': rate, 'amount': amount })
 
 def cancel_order(id):
-	return api_request('CancelOrder', {'order_id': id})
+    return api_request('CancelOrder', {'order_id': id})
